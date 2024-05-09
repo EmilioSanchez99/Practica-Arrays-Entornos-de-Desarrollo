1260d45a26b26bffbd0bad45c54ea56a843652f5
EmilioSanchez99 <emiliosv_99@hotmail.com>
2024-05-09 11:06:37 +0200

    cuarto metodo

--- PracticaArrays/src/Test/test5.java
+++ PracticaArrays/src/Test/test5.java
@@ -2,13 +2,35 @@ package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import util.MisArrays;
 
 class test5 {
 
+	static int[] notasBuenas;
+	static int[] notasMalas;
+
+	/**
+	 * instanciamos los arrays
+	 */
+	@BeforeAll
+	static void arrays() {
+		notasBuenas = new int[] { 2, 9, 6, 1, 6 };
+		notasMalas = new int[] { 13, 1, 12, 1, 9 };
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testMediana() {
+		int esperada = 6;
+		float obtenida = MisArrays.mediana(notasBuenas);
+		assertEquals(esperada, obtenida);
+	}
+
+	@Test
+	void testMedianaMal() {
+		assertThrows(IllegalArgumentException.class, () -> MisArrays.mediana(notasMalas));
 	}
 
 }
--- PracticaArrays/src/util/MisArrays.java
+++ PracticaArrays/src/util/MisArrays.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.Arrays;
+
 public abstract class MisArrays {
 
 	/**
@@ -7,54 +9,90 @@ public abstract class MisArrays {
 	 * @param array de notas
 	 * @return Devuelve la nota media obtenida de un array
 	 */
-	public static float mediaNotas(int[] array) throws IllegalArgumentException{
-		for (int i=0;i<array.length;i++) {
-			if (array[i]<0||array[i]>10) {
-				 throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
+	public static float mediaNotas(int[] array) throws IllegalArgumentException {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < 0 || array[i] > 10) {
+				throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
 			}
 		}
 		float sum = 0;
 		for (float nota : array) {
 			sum += nota;
-			}
+		}
 		return (float) sum / array.length;
-		}
-	
-	
-	public static int maximaNota(int[]array) {
-		
-		int max=array[0];
+	}
+
+	/**
+	 * 
+	 * @param array de notas
+	 * @return devuelve la nota maxima del array
+	 */
+	public static int maximaNota(int[] array) {
+
+		int max = array[0];
 		for (int i = 0; i < array.length; i++) {
-			if (array[i]<0||array[i]>10) {
-				 throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
+			if (array[i] < 0 || array[i] > 10) {
+				throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
+			} else {
+				if (array[i] > max) {
+					max = array[i];
+				}
 			}
-			else {
-				if(array[i]>max) {
-				max=array[i];
-			}
-			}
-			
+
 		}
-		
+
 		return max;
 	}
-	
-public static int minimaNota(int[]array) {
-		
-		int min=array[0];
+
+	/**
+	 * 
+	 * @param array de notas
+	 * @return devuelve la nota minima del array
+	 */
+	public static int minimaNota(int[] array) {
+
+		int min = array[0];
 		for (int i = 0; i < array.length; i++) {
-			if (array[i]<0||array[i]>10) {
-				 throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
+			if (array[i] < 0 || array[i] > 10) {
+				throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
+			} else {
+				if (array[i] < min) {
+					min = array[i];
+				}
 			}
-			else {
-				if(array[i]<min) {
-				min=array[i];
-			}
-			}
-			
+
 		}
-		
+
 		return min;
 	}
-	
+
+	/**
+	 * 
+	 * @param array
+	 * @return devuelve la mediana
+	 */
+	public static float mediana(int[] array) {
+
+		Arrays.sort(array);
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < 0 || array[i] > 10) {
+				throw new IllegalArgumentException("Las notas deben estar entre 0 y 10");
+			}
+		}
+		int tamaño = array.length;
+
+		float mediana;
+		if (tamaño % 2 != 0) {
+			mediana = array[tamaño / 2];
+		} else {
+			int midpos1 = tamaño / 2 - 1;
+			int midpos2 = tamaño / 2;
+			double valorCentral1 = array[midpos1];
+			double valorCentral2 = array[midpos2];
+			mediana = (float) ((valorCentral1 + valorCentral2) / 2);
+		}
+
+		return mediana;
+	}
+
 }
