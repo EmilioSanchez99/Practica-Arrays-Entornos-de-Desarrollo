8aa43926b924981017dc2abf40f1982b770cc3c4
EmilioSanchez99 <emiliosv_99@hotmail.com>
2024-05-09 10:48:16 +0200

    tercer metodo

--- PracticaArrays/src/Test/test4.java
+++ PracticaArrays/src/Test/test4.java
@@ -2,13 +2,34 @@ package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import util.MisArrays;
 
 class test4 {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
+	static int[]notasBuenas;
+	static int[]notasMalas;	
+	
+	/**
+	 * instanciamos los arrays
+	 */
+	@BeforeAll
+	static void arrays() {
+		notasBuenas= new int[]{2,9,6,1,6};
+		notasMalas =new int[]{2,1,12,5,9};			
+	}
+	
+	@Test
+	void testMinimaNota() {
+		int esperada=1;
+		int obtenida=MisArrays.minimaNota(notasBuenas);
+		assertEquals(esperada,obtenida);
+	}
+	@Test
+	void testNotaMinima(){
+		assertThrows(IllegalArgumentException.class,()->MisArrays.minimaNota(notasMalas));
+	}
 }
---
+++ PracticaArrays/src/Test/test5.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test5 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
